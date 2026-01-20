f226606a864ba38d7b4c96079472449afd61cdc5
Little Foot <littlefoot@mailfence.com>
2026-01-20 14:37:19 -0800

    fix: use initContent instead of deprecated initExtra

--- modules/home/littlefoot.nix
+++ modules/home/littlefoot.nix
@@ -18,7 +18,7 @@
       plugins = [ "git" "docker" "sudo" ];
     };
 
-    initExtra = ''
+    initContent = ''
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
       [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
     '';
