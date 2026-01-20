6c9fa4af36fcfa962bb1b5f648ff58988387e386
Little Foot <littlefoot@mailfence.com>
2026-01-20 13:38:34 -0800

    Add multiple terminal tools

--- modules/packages/terminals.nix
+++ modules/packages/terminals.nix
@@ -1,6 +1,16 @@
 { pkgs, ... }:
 
 [
+  pkgs.fzf
+  pkgs.htop
+  pkgs.httpie
   pkgs.kitty
+  pkgs.ncdu
+  pkgs.ripgrep
+  pkgs.rsync
+  pkgs.tldr
+  pkgs.tree
+  pkgs.unzip
   pkgs.yazi
+  pkgs.zip
 ]
